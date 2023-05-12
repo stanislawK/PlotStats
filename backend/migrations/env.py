cc0863ad4ab1fc80cc102bd3cbb29848645e472f
stanislawK <stanislaw.krupienko@gmail.com>
2023-05-12 08:42:04 +0200

    initial alembic revision

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -21,7 +21,6 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-# from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 target_metadata = SQLModel.metadata
 
