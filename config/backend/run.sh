6d712ba0a498f0956279e0374ccee4f2e006cdd9
stanislawK <stanislaw.krupienko@gmail.com>
2023-05-15 18:46:26 +0200

    run migrations when starting app

--- config/backend/run.sh
+++ config/backend/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 /scripts/wait-for-it.sh -t 15 $DB_HOST:$DB_PORT
+alembic upgrade head || exit 1
 
 if [ $DEBUG == True ]; then
     uvicorn api.app:create_app --host 0.0.0.0 --port 8000 --reload
