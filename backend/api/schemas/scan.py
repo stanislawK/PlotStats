0cfedd34fc046a5f8591315a1565cc4f1617cdc8
stanislawK <stanislaw.krupienko@gmail.com>
2023-09-12 23:55:32 +0200

    5 fetchning mutation

--- backend/api/schemas/scan.py
+++ backend/api/schemas/scan.py
@@ -2,10 +2,13 @@ from typing import Any
 
 import strawberry
 from pydantic import ValidationError
+from sqlalchemy.ext.asyncio import AsyncSession
 from strawberry.types import Info
 
+from api.parsing import parse_scan_data
 from api.types.general import InputValidationError
 from api.types.scan import AdhocScanInput, AdhocScanResponse, ScanFailedError
+from api.utils.fetching import make_request
 
 
 @strawberry.type
@@ -19,6 +22,11 @@ class Mutation:
         except ValidationError as error:
             return InputValidationError(message=str(error))
         print(f"Sending request to {data.url}...")
+        status_code, body = await make_request(data.url)
+        session: AsyncSession = info.context["session"]
+        await parse_scan_data(body, session)
+        print(status_code)
+        print(body)
         status_code = 404
         return ScanFailedError(
             message=f"Scan has failed with {status_code} status code."
---
+++ backend/api/utils/fetching.py
@@ -0,0 +1,25 @@
+import aiohttp
+
+from api.settings import settings
+
+HEADERS = {
+    "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+    "Referer": settings.base_url,
+    "Sec-Ch-Ua-Platform": '"Linux"',
+    "Sec-Ch-Ua-Mobile": "?0",
+    "Sec-Ch-Ua": '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
+    "Accept-Language": "en-US,en;q=0.9",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept": "*/*",
+}
+
+TOKEN_REGEX = r"(?<=buildId\":).+?(?=\",)"
+
+
+async def make_request(url: str) -> tuple[int, dict]:
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, headers=HEADERS) as resp:
+            if resp.status != 200:
+                return resp.status, {}
+            body = await resp.json()
+            return 200, body
