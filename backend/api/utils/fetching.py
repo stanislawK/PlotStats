ae16b89f79da986bb6b2ef8e7d0696e2a8accf66
stanislawK <stanislaw.krupienko@gmail.com>
2023-09-13 09:25:24 +0200

    5 - resolved linting changes

--- backend/api/utils/fetching.py
+++ backend/api/utils/fetching.py
@@ -1,9 +1,14 @@
+from typing import Any
+
 import aiohttp
 
 from api.settings import settings
 
 HEADERS = {
-    "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+    "user-agent": (
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 "
+        "(KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+    ),
     "Referer": settings.base_url,
     "Sec-Ch-Ua-Platform": '"Linux"',
     "Sec-Ch-Ua-Mobile": "?0",
@@ -16,7 +21,7 @@ HEADERS = {
 TOKEN_REGEX = r"(?<=buildId\":).+?(?=\",)"
 
 
-async def make_request(url: str) -> tuple[int, dict]:
+async def make_request(url: str) -> tuple[int, dict[str, Any]]:
     async with aiohttp.ClientSession() as session:
         async with session.get(url, headers=HEADERS) as resp:
             if resp.status != 200:
