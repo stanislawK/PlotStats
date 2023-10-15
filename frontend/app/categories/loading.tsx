c5f7095627c981321bd46a20b4025bc72baae733
Stanislaw <32984705+stanislawK@users.noreply.github.com>
2023-10-15 22:49:59 +0200

    52 add react (#57)
    * 52 add categories component

---
+++ frontend/app/categories/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="fixed top-5 ml-5 border rounded-md shadow-md p-5 w-64 bg-slate-50">
+      <p className="text-lg text-center">Loading ...</p>
+    </div>
+  );
+}
