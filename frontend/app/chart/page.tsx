910b4591d60a786006e9169e2de230ff9e413afa
stanislawK <stanislaw.krupienko@gmail.com>
2024-01-23 21:24:36 +0100

    96 - example charts

---
+++ frontend/app/chart/page.tsx
@@ -0,0 +1,23 @@
+import {
+  ExampleChart,
+  FlowbiteChart,
+  FlowbiteDemo,
+} from "../components/testChart";
+
+export default async function Charts() {
+  return (
+    <>
+      <div className="flex flex-col space-y-7 justify-center w-full h-full">
+        <div className="mx-auto">
+          <ExampleChart></ExampleChart>
+        </div>
+        <div className="mx-auto h-[55%]">
+          <FlowbiteChart></FlowbiteChart>
+        </div>
+        <div>
+          <FlowbiteDemo></FlowbiteDemo>
+        </div>
+      </div>
+    </>
+  );
+}
