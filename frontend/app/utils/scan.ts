4d2a562c3362d6e53e10723b369d0e45bf76b55d
stanislawK <stanislaw.krupienko@gmail.com>
2024-02-12 22:05:33 +0100

    119 User Facorit change FE

--- frontend/app/utils/scan.ts
+++ frontend/app/utils/scan.ts
@@ -42,9 +42,47 @@ export async function adhocScan(scan: Scan, accessToken: string) {
         Authorization: `Bearer ${accessToken}`,
       },
       body: mutation,
+      next: { revalidate: 0 },
     });
     const res_parsed = await api_res.json();
     const data = res_parsed.data["adhocScan"];
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+
+export async function addFavorite(id: string, accessToken: string) {
+  const mutation = JSON.stringify({
+    query: `
+    mutation assignFavoriteSearchToUser {
+      assignFavoriteSearchToUser(input: {id: ${id}}) {
+        ... on SearchAssignSuccessfully {
+          __typename
+          message
+        }
+        ... on SearchDoesntExistError {
+          __typename
+          message
+        }
+      }
+    }`,
+  });
+  try {
+    const api_res = await fetch("http://backend:8000/graphql", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-origin",
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: mutation,
+      next: { revalidate: 0 },
+    });
+    const res_parsed = await api_res.json();
+    const data = res_parsed.data["assignFavoriteSearchToUser"];
     return data;
   } catch (error) {
     console.log(error);
