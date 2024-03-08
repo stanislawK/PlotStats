2fded52fa4003b290e411a2361ddc04224db8834
Stanislaw <32984705+stanislawK@users.noreply.github.com>
2024-03-08 23:50:37 +0100

    144 - limit scope for user (#146)

--- frontend/app/dashboard/layout.tsx
+++ frontend/app/dashboard/layout.tsx
@@ -1,9 +1,12 @@
 import Sidebar from "../components/sidebar";
+import { getCookie } from "cookies-next";
+import { cookies } from "next/headers";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const accessToken = getCookie("accessToken", { cookies });
   return (
     <>
-      <Sidebar></Sidebar>
+      <Sidebar token={accessToken}></Sidebar>
       {children}
     </>
   );
