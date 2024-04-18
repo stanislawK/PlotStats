5aa20e8608c751c0c996d02e49c6cdbd1f820140
stanislawK <stanislaw.krupienko@gmail.com>
2024-04-18 21:55:17 +0200

    166 - hero page rewritten

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -38,7 +38,7 @@ function LinkBtn({ hasToken }: TokenProps) {
   return (
     <Link
       href={link}
-      className="bg-cyan-400 px-10 py-5 text-2xl font-bold text-white hover:opacity-70 md:py-4 rounded-full"
+      className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 hover:border-cyan-400 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
     >
       {msg}
     </Link>
@@ -58,30 +58,29 @@ export default function Home({ searchParams }: Props) {
     await activateAccount(data.email, data.tempPassword, data.newPassword);
   };
   return (
-    <main className="flex flex-col-reverse mb-10 mx-auto space-y-5 md:flex-row md:space-y-0 items-center md:mx-28 md:mt-10">
-      {/* Content container */}
-      <div className="flex flex-col mt-10 space-y-10 md:w-1/2 md:pl-10">
-        <h1 className="text-center text-5xl font-bold md:text-left md:max-w-md">
-          Unleash Data Insights
-        </h1>
-        <p className="text-center text-2xl text-gray-500 md:max-w-md md:text-left">
-          Explore the dynamic world of real estate with our powerful dashboard.
-          Instantly track and analyze price changes for apartments, plots, and
-          houses. Your key to informed decisions in the ever-evolving property
-          market.
-        </p>
-        <div className="mx-auto md:mx-0">
+    <section className="bg-white dark:bg-gray-900">
+      <div className="grid max-w-screen-xl h-screen px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+        <div className="mr-auto place-self-center lg:col-span-7">
+          <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
+            Unleash Data Insights
+          </h1>
+          <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+            Explore the dynamic world of real estate with our powerful
+            dashboard. Instantly track and analyze price changes for apartments,
+            plots, and houses. Your key to informed decisions in the
+            ever-evolving property market.
+          </p>
           <LinkBtn hasToken={hasToken}></LinkBtn>
         </div>
-      </div>
-      <div className="mx-auto mb-24 md:w-1/2">
-        <Image
-          src="/undraw_projections_re_ulc6.svg"
-          alt="Dashboard Logo"
-          width={1000}
-          height={240}
-          priority
-        />
+        <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
+          <Image
+            src="/undraw_projections_re_ulc6.svg"
+            alt="Dashboard Logo"
+            width={1000}
+            height={240}
+            priority
+          />
+        </div>
       </div>
       {/* Login modal */}
       {showLoginModal && <LoginModal authenticate={loginFunc} />}
@@ -91,6 +90,6 @@ export default function Home({ searchParams }: Props) {
           activateAccountFunc={activateUserFunc}
         ></ActivateAccountModal>
       )}
-    </main>
+    </section>
   );
 }
