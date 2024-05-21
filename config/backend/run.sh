d47d27703429a575c6db158300a7ce022ec1b9a8
Stanislaw <32984705+stanislawK@users.noreply.github.com>
2024-05-21 22:44:15 +0200

    181 - limit image size (#182)

--- config/backend/run.sh
+++ config/backend/run.sh
@@ -6,5 +6,5 @@ alembic upgrade head || exit 1
 if [ $DEBUG == True ]; then
     uvicorn api.asgi:app --host 0.0.0.0 --port 8000 --reload
 else
-    gunicorn api.asgi:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
+    gunicorn api.asgi:app --workers 2 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
 fi
