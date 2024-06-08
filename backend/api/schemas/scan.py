16de4a3db608d15b4382a5ac7b6ee16299a44157
Stanislaw <32984705+stanislawK@users.noreply.github.com>
2024-06-08 22:31:56 +0200

    209 - reuse vailable cookies (#212)

--- backend/api/schemas/scan.py
+++ backend/api/schemas/scan.py
@@ -35,7 +35,7 @@ class Mutation:
             return InputValidationError(message=str(error))
         base_url = str(data.url)
         url = parse_url(base_url)
-        status_code, body = await make_request(url)
+        status_code, body, req_session = await make_request(url)
         session: AsyncSession = info.context["session"]
         if status_code != 200:
             await handle_failed_scan(status_code, url, base_url, session)
@@ -58,8 +58,10 @@ class Mutation:
             return ScanSucceeded  # type: ignore
         for page_number in range(2, total_pages + 1):
             next_url = url + f"&page={page_number}"
-            status_code, body = await make_request(
-                url=next_url, wait_before_request=random.randint(10, 20)
+            status_code, body, req_session = await make_request(
+                url=next_url,
+                wait_before_request=random.randint(10, 20),
+                session=req_session,
             )
             if status_code != 200:
                 await handle_failed_scan(status_code, next_url, base_url, session)
