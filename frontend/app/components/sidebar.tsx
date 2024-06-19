1b745653c8fd9f76ce821a65bf4326cadc614023
stanislawK <stanislaw.krupienko@gmail.com>
2024-06-19 23:35:31 +0200

    222 - active users tab

--- frontend/app/components/sidebar.tsx
+++ frontend/app/components/sidebar.tsx
@@ -160,7 +160,7 @@ export default function Sidebar({ token }: Props) {
               <li>
                 <Link
                   href="/dashboard/users"
-                  className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                  className={`flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group ${pathname === "/dashboard/users" ? "bg-gray-200" : ""}`}
                 >
                   <svg
                     className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
