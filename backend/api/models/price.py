4274a06695379dd8102cbfc9fe802757e8584a15
Stanislaw <32984705+stanislawK@users.noreply.github.com>
2025-03-17 00:51:55 +0100

    243 - estate id to bigint (#244)

--- backend/api/models/price.py
+++ backend/api/models/price.py
@@ -1,5 +1,6 @@
 from typing import TYPE_CHECKING, Optional
 
+from sqlalchemy import BigInteger, Column, ForeignKey
 from sqlmodel import Field, Relationship, SQLModel
 
 from .estate import Estate
@@ -14,7 +15,9 @@ class Price(SQLModel, table=True):
     price_per_square_meter: Optional[int] = Field(default=None)
     area_in_square_meters: Optional[int] = Field(default=None)
     terrain_area_in_square_meters: Optional[int] = Field(default=None)
-    estate_id: Optional[int] = Field(default=None, foreign_key="estate.id")
+    estate_id: Optional[int] = Field(
+        default=None, sa_column=Column(BigInteger(), ForeignKey("estate.id"))
+    )
     estate: Estate = Relationship(back_populates="prices")
     search_event_id: Optional[int] = Field(default=None, foreign_key="searchevent.id")
     search_event: "SearchEvent" = Relationship(back_populates="prices")
